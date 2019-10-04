c2d69ab70d8874bb494f16de95ac0cfc8febe19c
Denis Vrdoljak <denisvrdoljak@yahoo.com>
2019-10-04 02:02:23 -0700

    week 2 notes and examples

---
+++ wk2/hw2_dvrdoljak.sh
@@ -0,0 +1,8 @@
+# This is an example of a shell script
+# (comments start with a #, just like Python comments)
+touch helloworld.txt
+mkdir location1
+mv helloworld.txt location1/
+rm -rf location1
+echo '' > file2.txt
+
